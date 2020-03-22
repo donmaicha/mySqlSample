22dd0013ba7fcb75a96ada45fbd28abc73125128
donmaicha <genmaicha77783@gmail.com>
2020-03-22 23:07:53 +0900

    うまくmigrationできないンゴ...

---
+++ QuickStart/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace QuickStart.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ QuickStart/Models/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuickStart.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
